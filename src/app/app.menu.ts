3d7823ebacf2379cad78e5bad91a82e17837d153
Carlos Pons <karlospn@gmail.com>
2017-03-19 20:01:21 +0100

    Create contry-detail, country-list and count-maint components

--- src/app/app.menu.ts
+++ src/app/app.menu.ts
@@ -11,13 +11,70 @@ export let menuItemsConfig: Array<MenuItem> =
     {
         text: 'Countries',
         icon: 'glyphicon-flag',
-        route: '/countries',
-        subMenu: null
+        route: null,
+        subMenu: [
+            {
+                text: 'Select',
+                icon: 'glyphicon-expand',
+                route: null,
+                subMenu: [
+                    {
+                        text: 'USA',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/USA',
+                        subMenu: null
+                    },
+                    {
+                        text: 'India',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/India',
+                        subMenu: null
+                    },
+                    {
+                        text: 'Switzerland',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/Switzerland',
+                        subMenu: null
+                    }
+                ]
+            },
+            {
+                text: 'Top 3',
+                icon: 'glyphicon-flag',
+                route: 'country-list/3',
+                subMenu: null
+            },
+            {
+                text: 'Top 10',
+                icon: 'glyphicon-flag',
+                route: 'country-list/10',
+                subMenu: null
+            },
+            {
+                text: 'All',
+                icon: 'glyphicon-flag',
+                route: 'country-list/0',
+                subMenu: null
+            }
+        ]
     },
     {
-        text: 'Settings',
+        text: 'Maintenance',
         icon: 'glyphicon-wrench',
-        route: '/settings',
-        subMenu: null
+        route: null,
+        subMenu: [         
+            {
+                text: 'Country List',
+                icon: 'glyphicon-th-list',
+                route: 'country-maint',
+                subMenu: null
+            },
+            {
+                text: 'Settings',
+                icon: 'glyphicon-cog',
+                route: 'settings',
+                subMenu: null
+            }
+        ]
     }
 ]
