ddea42d0780da0a0664fb90bf181855c8c887514
Carlos Pons <karlospn@gmail.com>
2017-03-26 16:47:36 +0200

    Create sign-out method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { UserApi } from '../../fw/users/user-api';
 import { Injectable} from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Rx';
 
@@ -7,13 +8,26 @@ import { Observable } from 'rxjs/Rx';
 export class UserService implements UserApi {
 
     public isAuthenticated : boolean = false;
+    public userName : string;
     
-    constructor(){}
+    constructor(private router : Router){}
     
     signIn(username: string, password: string, rememberMe: boolean): Observable<any> {
         this.isAuthenticated = true;
+        this.userName = username;
         return Observable.of({});
     }
 
+
+    signOut() : Observable<any>{
+        this.isAuthenticated = false;
+        this.userName = null;
+        this.router.navigate(['/signin'])
+        return Observable.of({});
+    }
+
+    getUsername(): string {
+        return this.userName;
+    }
 
 }
--- src/fw/top-bar/top-bar.component.ts
+++ src/fw/top-bar/top-bar.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FrameworkConfigService } from '../service/framework-config.service';
+import { UserApi } from '../users/user-api';
 
 @Component({
   selector: 'fw-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent {
+export class TopBarComponent implements OnInit {
 
-  constructor(private frameWorkConfigService: FrameworkConfigService) { }
+  private username : string;
+
+  constructor(private frameWorkConfigService: FrameworkConfigService, 
+              private userApi: UserApi) { }
+
+  ngOnInit(): void {
+      this.username = this.userApi.getUsername();
+  }
+
+
+  signOut(){
+    this.userApi.signOut();
+  }
 
 }
