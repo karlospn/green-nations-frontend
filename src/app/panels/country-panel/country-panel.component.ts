72af764798e51fe19b5ef62703a91d5518c2ca3d
karlospn <karlospn@gmail.com>
2017-03-27 10:48:26 +0200

    Implement country panel component

--- src/app/panels/country-panel/country-panel.component.ts
+++ src/app/panels/country-panel/country-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Country } from '../../view-models/country';
 
 @Component({
   selector: 'app-country-panel',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-panel.component.css']
 })
 export class CountryPanelComponent implements OnInit {
+
+  @Input() country: Country;
+  @Input() index = 1;
 
   constructor() { }
 
